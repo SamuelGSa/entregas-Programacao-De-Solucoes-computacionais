a381a467c8c06a56d203aac28d2b5314656bc1f3
SamuelGSa <sgmsa@icloud.com>
2024-03-18 18:27:24 -0300

    atividade dia 11-03

--- entregas11-03/src/main/java/org/example/Ex002.java
+++ entregas11-03/src/main/java/org/example/Ex002.java
@@ -2,39 +2,40 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Ex001 {
+public class Ex002 {
 //    Entrada com a quantidade de cada pão;
 //    Saída com o número de cada tipo e pão e seu respectivo valor de venda;
 //    Saída com o valor total da compra supondo que um mesmo cliente comprou vários tipos de pães.
 
     public static void main(String[] args) {
 
-        var custoTotalPaoIntegral = custoTotal(0.50,0.20,0.10);
-        var custoTotalPaoDoceLiso = custoTotal(0.50,0.20,0.10);
-        var custoTotalPaoDoceFarofa = custoTotal(0.50,0.20,0.10);
-        var custoTotalPaoShibata = custoTotal(0.50,0.20,0.10);
+        var custoTotalPaoFrances = custoTotal(0.51,0.20,0.10);
+        var custoTotalPaoIntegral = custoTotal(0.49,0.20,0.12);
+        var custoTotalPaoDoceLiso = custoTotal(0.53,0.20,0.09);
+        var custoTotalPaoDoceFarofa = custoTotal(0.55,0.20,0.10);
+        var custoTotalPaoShibata = custoTotal(0.45,0.20,0.08);
 
-        System.out.println("Pao integral - Valor De Venda: " + valorVenda(custoTotalPaoIntegral,20,10));
+        System.out.println("Pao Frances - Valor De Venda: " + valorVenda(custoTotalPaoFrances,20,9));
+        System.out.println("Pao Frances - Custo Total: " + custoTotalPaoFrances);
+
+        System.out.println("Pao integral - Valor De Venda: " + valorVenda(custoTotalPaoIntegral,20,8.5));
         System.out.println("Pao integral - Custo Total: " + custoTotalPaoIntegral);
 
-        System.out.println("Pao Doce Liso - Valor De Venda: " + valorVenda(custoTotalPaoDoceLiso,20,10));
+        System.out.println("Pao Doce Liso - Valor De Venda: " + valorVenda(custoTotalPaoDoceLiso,20,12));
         System.out.println("Pao Doce Liso - Custo Total: " + custoTotalPaoDoceLiso);
 
-        System.out.println("Pao Doce Farofa - Valor De Venda: " + valorVenda(custoTotalPaoDoceFarofa,20,10));
+        System.out.println("Pao Doce Farofa - Valor De Venda: " + valorVenda(custoTotalPaoDoceFarofa,20,11));
         System.out.println("Pao Doce Farofa - Custo Total: " + custoTotalPaoDoceFarofa);
 
         System.out.println("Pao Shibata - Valor De Venda: " + valorVenda(custoTotalPaoShibata,20,10));
         System.out.println("Pao Shibata - Custo Total: " + custoTotalPaoShibata);
     }
 
-    public static Scanner scanner(){
-        return new Scanner(System.in);
-    }
     public static double custoTotal(double ingredientes, double maoDeObra, double despesasFixas){
         return  (ingredientes + maoDeObra + despesasFixas);
     }
 
-    public static double valorVenda(double custoTotal,int lucroDesejadoPorcent,int margemSegurancaPorcent){
+    public static double valorVenda(double custoTotal,int lucroDesejadoPorcent,double margemSegurancaPorcent){
         return  (custoTotal + ((custoTotal /100 * lucroDesejadoPorcent)) + ((custoTotal / 100 * margemSegurancaPorcent)));
     }
 }
--- entregas11-03/src/main/java/org/example/Main.java
+++ entregas11-03/src/main/java/org/example/Main.java
@@ -1,8 +1,42 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int int1,int2;
+        double real1,real2;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro numero inteiro: ");
+        int1 = scanner.nextInt();
+
+        System.out.print("Digite o segundo numero inteiro: ");
+        int2 = scanner.nextInt();
+
+        System.out.print("Digite o primeiro numero real: ");
+        real1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo numero real: ");
+        real2 = scanner.nextDouble();
+
+        int somaInt = int1 + int2;
+
+        double somaReal = real1 + real2;
+
+        double prodReal =  real1 * real2;
+
+        double divReal = real1 / real2;
+
+        System.out.println("Operação A: " + somaInt);
+        System.out.println("Operação B: " + somaReal);
+        System.out.println("Operação C: " + prodReal);
+
+        if (real2 != 0) System.out.println("Operação D: " + divReal);
+        else System.out.println("Operação D: Divisão Impossivel");
+
 
     }
 }
