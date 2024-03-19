822d877220ec165aa4a6ff9888bb58dad7b9e502
SamuelGSa <sgmsa@icloud.com>
2024-03-19 12:02:53 -0300

    att

--- entregas18-03/src/main/java/org/example/Main.java
+++ entregas18-03/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println( "Link para a atividade: " + "https://drive.google.com/drive/folders/1hn2f-Rp4V1X9aIXi2QVrLwvKYhDGo3SL");
     }
 }
