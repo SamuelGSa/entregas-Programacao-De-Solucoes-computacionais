7db27b3f81d87587001fe9d44abd2f370b2c12ec
SamuelGSa <sgmsa@icloud.com>
2024-04-08 19:41:18 -0300

    fluxograma

--- entregas25-03/src/Main.java
+++ entregas25-03/src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+
+//    Pessoal material da aula de hoje, não esquecer de "tentar" fazer fluxograma da atividade 4.
+//    https://docs.google.com/document/d/1fAFBA1JEXE3_19Xf4gqO6ZoJ3hkiKrkibAhkU32T6pQ/edit
+//    Abraços
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
