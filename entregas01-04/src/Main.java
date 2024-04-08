2c8df1a3be928279d372d9b1cb4f85f06145f788
SamuelGSa <sgmsa@icloud.com>
2024-04-08 19:35:53 -0300

    a

---
+++ entregas01-04/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+//    Boa noite pessoal
+//    Como atividade para aula de hoje sugiro que convertam os códigos já explicados em PORTUGOL para JAVA,
+//    usando CHATGPT ou GEMINI, rodem em (https://www.onlinegdb.com/online_java_compiler) compilador online JAVA e
+//    verifiquem como são os formatos de codificação em relação a forma de escrever.
+//   https://drive.google.com/drive/folders/1WNGkktzgYe16dZGpnRNrctrCgB8ohsj2?usp=drive_link
+//   https://docs.google.com/document/d/1OvoAEtz28DYswxUA_qazQXmGrZxnLX94GybjwEmBpCw/edit?usp=sharing
+    public static void main(String[] args) {
+
+        System.out.println("TODAS AS ATIVIDADES FORAM CONVERTIDAS PARA JAVA. BUSCAR NESTE REPOSITORIO QUAL ATIVIDADE DESEJADA!");
+    }
+}
---
+++ entregas26-03/src/Ex001.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex001 {
+
+    public static void main(String[] args) {
+        int opcao = -1;
+
+
+    }
+
+    public static Scanner scanner() {
+        return new Scanner(System.in);
+    }
+}
